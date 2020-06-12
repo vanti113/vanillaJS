3a0fc2d0454e82e28f91d96cecd0221778c1ac57
vanti113 <vanti113@gmail.com>
2020-06-12 14:04:00 +0900

    let's start

--- class9,10/src/index.js
+++ class9,10/src/index.js
@@ -8,7 +8,6 @@ const saveNum = {
   first: "",
   second: "",
   oper: "",
-  result: "",
 };
 
 function clear() {
@@ -18,8 +17,8 @@ function clear() {
   input.value = "";
 }
 
-function prepareNxt() {
-  saveNum.first = saveNum.result;
+function prepareNxt(result) {
+  saveNum.first = result;
   saveNum.second = "";
 }
 
@@ -44,28 +43,7 @@ function processCal() {
       break;
   }
 
-  saveNum.result = result.toString(); // 나갈때는 문자열로 변환해야 함.
-}
-
-function operFunc(e) {
-  const tempData = e.target.innerText;
-
-  if (saveNum.first !== "") {
-    // 첫번째 수가 존재한다면
-    saveNum.oper = tempData;
-
-    if (saveNum.second !== "" && saveNum.oper !== "") {
-      // 두번째 수가 존재한다면
-      //첫째 수와 둘째 수를 연산하는 함수를 호출하고 그 리턴 값을 표시하라.
-      processCal();
-      input.value = saveNum.result;
-      saveNum.oper = tempData;
-      prepareNxt();
-    } else {
-      // 두번째 수가 존재하지 않으면
-      saveNum.oper = tempData;
-    }
-  }
+  return result.toString(); // 나갈때는 문자열로 변환해야 함.
 }
 
 function check(e) {
@@ -83,22 +61,40 @@ function check(e) {
       input.value = saveNum.second;
     }
   }
-
+  //두번째 페이즈는 첫 수가 등록되었고, 연산자도 있을 경우, 계산을 위한 두번째 수를 객체에 저장한다. 그리고 새롭게 인풋에 표시한다.
+  if (className === "oper") {
+    // 연산자 키가 눌렸을 경우
+    if (saveNum.first !== "") {
+      // 첫번째 수가 존재한다면
+      saveNum.oper = tempData;
+      input.value = saveNum.first;
+      if (saveNum.second !== "") {
+        // 두번째 수가 존재한다면
+        //첫째 수와 둘째 수를 연산하는 함수를 호출하고 그 리턴 값을 표시하라.
+        const result = processCal();
+        input.value = result;
+        saveNum.oper = tempData;
+        prepareNxt(result);
+      } else {
+        // 두번째 수가 존재하지 않으면
+        saveNum.oper = tempData;
+      }
+    }
+    input.value = saveNum.second;
+    console.log(saveNum.second);
+  }
   if (className === "equal") {
     if (saveNum.first !== "" && saveNum.second !== "" && saveNum.oper !== "") {
-      processCal();
-      input.value = saveNum.result;
+      const result = processCal();
+      input.value = result;
       saveNum.oper = "";
-      prepareNxt();
+      prepareNxt(result);
     }
   }
 
   if (className === "clear") {
     clear();
   }
-  console.log(
-    `${saveNum.first} | ${saveNum.second} | ${saveNum.oper} | ${input.value}`
-  );
 }
 
 function init() {
@@ -108,7 +104,7 @@ function init() {
   }
   for (let i = 0; i < operBtn.length; i++) {
     //연산자 키에 대한 이벤트 핸들러 등록
-    operBtn[i].addEventListener("click", operFunc);
+    operBtn[i].addEventListener("click", check);
   }
   clBtn.addEventListener("click", check);
   eqBtn.addEventListener("click", check);
