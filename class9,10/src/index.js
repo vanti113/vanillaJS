541a067b5e5adb8759595835df193bff98941197
vanti113 <vanti113@gmail.com>
2020-06-12 14:34:34 +0900

    case1

--- class9,10/src/index.js
+++ class9,10/src/index.js
@@ -46,11 +46,32 @@ function processCal() {
   return result.toString(); // 나갈때는 문자열로 변환해야 함.
 }
 
+// function operFunc(e) {
+//   const tempData = e.target.innerText;
+
+//   if (saveNum.first !== "") {
+//     // 첫번째 수가 존재한다면
+//     saveNum.oper = tempData;
+
+//     if (saveNum.second !== "") {
+//       // 두번째 수가 존재한다면
+//       //첫째 수와 둘째 수를 연산하는 함수를 호출하고 그 리턴 값을 표시하라.
+//       const result = processCal();
+//       input.value = result;
+//       saveNum.oper = tempData;
+//       prepareNxt(result);
+//     } else {
+//       // 두번째 수가 존재하지 않으면
+//       saveNum.oper = tempData;
+//     }
+//   }
+// }
+
 function check(e) {
   // 버튼이 눌리면 버튼의 이너텍스트 값을 임시 변수에 등록한다.
   const tempData = e.target.innerText;
   const className = e.target.className; // 버튼의 클래스네임을 구분자로 사용
-
+  e.preventDefault();
   // 조건문을 이용한다. 첫번째 페이즈에서 유저는 첫 수를 입력하고, 그 수는 객체에 저장되어 인풋의 밸류에 나타나게 됨
   if (className === "number") {
     if (saveNum.oper === "") {
@@ -62,12 +83,20 @@ function check(e) {
     }
   }
   //두번째 페이즈는 첫 수가 등록되었고, 연산자도 있을 경우, 계산을 위한 두번째 수를 객체에 저장한다. 그리고 새롭게 인풋에 표시한다.
+
+  if (className === "equal") {
+    if (saveNum.first !== "" && saveNum.second !== "" && saveNum.oper !== "") {
+      const result = processCal();
+      input.value = result;
+      saveNum.oper = "";
+      prepareNxt(result);
+    }
+  }
   if (className === "oper") {
-    // 연산자 키가 눌렸을 경우
     if (saveNum.first !== "") {
       // 첫번째 수가 존재한다면
       saveNum.oper = tempData;
-      input.value = saveNum.first;
+
       if (saveNum.second !== "") {
         // 두번째 수가 존재한다면
         //첫째 수와 둘째 수를 연산하는 함수를 호출하고 그 리턴 값을 표시하라.
@@ -80,21 +109,15 @@ function check(e) {
         saveNum.oper = tempData;
       }
     }
-    input.value = saveNum.second;
-    console.log(saveNum.second);
-  }
-  if (className === "equal") {
-    if (saveNum.first !== "" && saveNum.second !== "" && saveNum.oper !== "") {
-      const result = processCal();
-      input.value = result;
-      saveNum.oper = "";
-      prepareNxt(result);
-    }
   }
 
   if (className === "clear") {
     clear();
   }
+
+  console.log(
+    `fisrt : ${saveNum.first} | second : ${saveNum.second} | oper : ${saveNum.oper} | input : ${input.value}`
+  );
 }
 
 function init() {
